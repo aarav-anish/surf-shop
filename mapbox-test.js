8bc3b978b9a5c1bf4f565d71a96b475e0ba2f0f3
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2021-08-31 23:32:08 +0530

    Add map to Post show view

---
+++ mapbox-test.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
+
+const rgeocoder = async (coordinates) => {
+  try {
+    let response = await geocodingClient
+      .reverseGeocode({
+        query: coordinates,
+        limit: 1,
+      })
+      .send();
+    console.log(response.body);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const fgeocoder = async (location) => {
+  try {
+    let response = await geocodingClient
+      .forwardGeocode({
+        query: location,
+        limit: 1,
+      })
+      .send();
+
+    console.log(response.body.features[0].geometry.coordinates);
+    console.log('');
+    rgeocoder(response.body.features[0].geometry.coordinates);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+fgeocoder('Dhanbad, India');
